004c976b0b1e334d554383ddb13c7ac2311a6619
YerkoFC <yerko.fonceac@utem.cl>
2020-08-07 20:51:21 -0400

    Se modifica operacion 2

--- rutas/autenticacion.js
+++ rutas/autenticacion.js
@@ -1,32 +1,46 @@
+// ==========================
+// express
+// ==========================
 const express = require('express');
 const app = express();
 
+// ==========================
+// jsonwebtoken
+// ==========================
 const jwt = require('jsonwebtoken');
 
+// ==========================
+// Archivo de usuarios
+// ==========================
 let userFile = require('../resources/usuarios.json');
 
 app.post('/autenticacion', (req, res) => {
 
+    // Se obtienen las credenciales a través del body de la petición
     let body = req.body;
-    let users = userFile;
 
+    // Se obtienem los usuarios del archivo usuarios.json
+    let users = userFile;
+    
+    // Si las credenciales de usuario ingresadas coinciden, se procede a crear un token
     users.forEach((user) => {
-        if (body.usuario === user.usuario && body.password == user.password) {
+        if (body.usuario == user.usuario && body.password == user.password) {
 
             // Genera un token
             let token = jwt.sign({ usuario: body.usuario },
                 process.env.SEED_JWT, { expiresIn: process.env.EXPIRATION_TOKEN });
-
-            res.status(200).json({
+            
+            // Se envía el token a través de un formato json
+            res.json({
                 ok: true,
                 token
             });
 
-        } else {
-            res.status(400).json({
+        } else { // Si las credenciales no coinciden, el servidor responde con un codigo 401 indicando que el usuario no existe o no está autorizada
+            res.status(401).json({
               ok: false,
               err: {
-                  message: 'No existe usuario asociado a las credenciales ingresadas'
+                  mensaje: 'No existe usuario asociado a las credenciales ingresadas'
               }
             });
         }
--- server/server.js
+++ server/server.js
@@ -1,22 +1,33 @@
+// ==========================
 // Modulos requeridos
+// ==========================
 const express = require('express');
 
-// Configuración del server
+// ==========================
+// Variables de entorno
+// ==========================
 process.env.PORT = process.env.PORT || 3000;
 process.env.SEED_JWT = process.env.SEED_JWT || 'mi-llave-secreta';
 process.env.EXPIRATION_TOKEN = process.env.EXPIRATION_TOKEN || 60 * 60 * 24 * 30; 
 
+
 const app = express();
 
+// ==========================
 // Middlewares para que el servidor entienda archivos json
+// ==========================
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// ==========================
 // Rutas
-app.use(require('../rutas/carreras.js'));
-app.use(require('../rutas/autenticacion'));
+// ==========================
+app.use('/api', require('../rutas/carreras.js'));
+app.use('/api', require('../rutas/autenticacion'));
 
-// Iniciación del servidor
+// ==========================
+// Iniciación del servidor en un puerto establecido
+// ==========================
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto ${process.env.PORT}`);
 });
